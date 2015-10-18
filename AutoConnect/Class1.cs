db06ca56f059a0433feaadeb07bae06a064bd8ac
Patrick Hao <haoxiangpeng123@gmail.com>
2015-10-18 21:19:19 +0800

    1.增加了后台连接的功能，当网络状态改变时，自动连接（Beta） 2.增加了诸多细节，改善用户体验。

---
+++ AutoConnect/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Windows .ApplicationModel .Background ;
+
+
+namespace AutoConnect
+{
+    public sealed class AutoConnect:IBackgroundTask
+    {
+        public async  void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral _deferral = taskInstance.GetDeferral();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string postData =
+                "action=login&username=" + "4578" + "&password=" + "4564" + "&ac_id=3&type=1&mac=undefined&user_ip=&is_ldap=1&local_auth=1";
+            byte[] data = encoding.GetBytes(postData);
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("https://net.zju.edu.cn/cgi-bin/srun_portal");
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+
+
+            Stream newStream = await myRequest.GetRequestStreamAsync();
+
+            newStream.Write(data, 0, data.Length);
+            newStream.Dispose();
+
+            WebResponse myResponse = await myRequest.GetResponseAsync();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream());
+            string content = reader.ReadToEnd();
+            var dialog = new Windows.UI.Popups.MessageDialog(content, "ZJUWlan");
+            await dialog.ShowAsync();
+            myResponse.Dispose();
+            _deferral.Complete();
+        }
+    }
+}
--- ZJUWlan/MainPage.xaml.cs
+++ ZJUWlan/MainPage.xaml.cs
@@ -7,8 +7,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,6 +34,27 @@ namespace ZJUWlan
             this.InitializeComponent();
         }
 
+        public void background()
+        {
+            var taskRegistered = false;
+            var myTaskName = "AutoConnect";
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == myTaskName) ;
+                {
+                    taskRegistered = true;
+                    break;
+                }
+            }
+            if (!taskRegistered)
+            {
+                var builder = new BackgroundTaskBuilder();
+                builder.Name = myTaskName;
+                builder.TaskEntryPoint = "AutoConnect.AutoConnect";
+                builder.SetTrigger(new SystemTrigger(SystemTriggerType.NetworkStateChange, false));
+                BackgroundTaskRegistration task = builder.Register();
+            }
+        }
         public async Task<bool> SaveFile(string usr, string pwd)
         {
             try
@@ -53,12 +76,12 @@ namespace ZJUWlan
 
         public async Task<string> ReadFile()
         {
-            string info =string.Empty ;
+            string info = string.Empty;
             try
             {
                 StorageFolder loadFolder = ApplicationData.Current.LocalFolder;
                 StorageFile loadFile = await loadFolder.GetFileAsync("info.txt");
-                
+
                 info = await FileIO.ReadTextAsync(loadFile);
                 return info;
             }
@@ -67,19 +90,10 @@ namespace ZJUWlan
                 e.ToString();
                 return info;
             }
-        } 
+        }
 
-
-
-        private async void button_Click(object sender, RoutedEventArgs e)
+        public async void ConnectToWlan(string strID,string strPassword)
         {
-
-            string strID = textBox.Text;
-            string strPassword = passwordBox .Password ;
-            if(checkBox .IsChecked.Value  )
-            {
-                bool fileState = await SaveFile(strID, strPassword);
-            }
             ASCIIEncoding encoding = new ASCIIEncoding();
             string postData =
                 "action=login&username=" + strID + "&password=" + strPassword + "&ac_id=3&type=1&mac=undefined&user_ip=&is_ldap=1&local_auth=1";
@@ -98,24 +112,62 @@ namespace ZJUWlan
             WebResponse myResponse = await myRequest.GetResponseAsync();
             StreamReader reader = new StreamReader(myResponse.GetResponseStream());
             string content = reader.ReadToEnd();
-            var dialog = new Windows.UI.Popups.MessageDialog(content , "ZJUWlan");
-            dialog.ShowAsync();
+            var dialog = new Windows.UI.Popups.MessageDialog(content, "ZJUWlan");
+            await dialog.ShowAsync();
             myResponse.Dispose();
         }
 
-        private async  void Grid_Loaded(object sender, RoutedEventArgs e)
+
+
+
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
-            string info = string.Empty ;
-            info =await  ReadFile();
-            textBox.Text = info.Substring( 0,info.IndexOf("\r\n", StringComparison.Ordinal));
-           passwordBox .Password  = info.Substring( info .IndexOf( "\r\n", StringComparison.Ordinal)+2,info.Length- (info.IndexOf("\r\n", StringComparison.Ordinal) + 2));
             
+            string strID = textBox.Text;
+            string strPassword = passwordBox.Password;
+            if (checkBox.IsChecked.Value)
+            {
+                bool fileState = await SaveFile(strID, strPassword);
+            }
+            if (wifiInfo.IsWlanConnectionProfile && wifiInfo.ProfileName == "ZJUWLAN")
+                ConnectToWlan(strID, strPassword);
+            else
+            {
+                var dialog = new Windows.UI.Popups.MessageDialog("先连上ZJU的WIFI哦~", "ZJUWlan");
+                await dialog.ShowAsync();
+            }
+
         }
 
-        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            var dialog = new Windows.UI.Popups.MessageDialog("由于ZJU自己都没有搞清楚怎么弄，该功能暂不开放。异地登陆即可注销当前","ZJUWlan");
-            dialog.ShowAsync();
+            string info = string.Empty;
+            info = await ReadFile();
+            textBox.Text = info.Substring(0, info.IndexOf("\r\n", StringComparison.Ordinal));
+            passwordBox.Password = info.Substring(info.IndexOf("\r\n", StringComparison.Ordinal) + 2, info.Length - (info.IndexOf("\r\n", StringComparison.Ordinal) + 2));
+            GetWifiInfo();
+        }
+        ConnectionProfile wifiInfo = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+        public void GetWifiInfo()
+        {
+            
+            if (wifiInfo.IsWlanConnectionProfile)
+                wificonn_textblock.Text = "Connected";
+            else
+                wificonn_textblock.Text = "Not Connected";
+            wifiProfil_txtblock.Text = wifiInfo.ProfileName;
+        }
+
+
+        private async void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog("由于ZJU自己都没有搞清楚怎么弄，该功能暂不开放。异地登陆即可注销当前", "ZJUWlan");
+            await dialog.ShowAsync();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            background();
         }
     }
 }
